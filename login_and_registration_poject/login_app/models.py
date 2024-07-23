11df6ccf82f613939c03236258ddfb00276c62bb
Reem Joudeh <joudehreem@gmail.com>
2024-07-23 12:21:33 +0300

    final

--- login_and_registration_poject/login_app/models.py
+++ login_and_registration_poject/login_app/models.py
@@ -9,28 +9,28 @@ class UserManager(models.Manager):
     def basic_register(self, postData): # function for registration 
         errors = {}
         if len(postData['first_name']) < 2:# validated first name
-            errors["first_name"] = []## as list ""if satament
-            errors["first_name"].append=('')
+            errors["first_name"] = "First Name should be at least 2 characters"## as list ""if satament
+            # errors["first_name"].append=('')
         if len(postData['last_name']) < 2:# validated last name
             errors["last_name"] = "Last Name should be at least 2 characters"
         # validated dob to required in database and age grater than 13
-        dob = datetime.strptime(postData['dob'], "%Y-%m-%d").date()
-        today = datetime.now().date()
         if not postData['dob']:
             errors["dob"] = "Date of Birth is required"
         else:
-            try:
+            # try:
                 dob = datetime.strptime(postData['dob'], "%Y-%m-%d").date()
+                today = datetime.now().date()
+                age = today.year - dob.year
                 if dob >= today:
-                    errors["dob"] = "The Date of Birth must be in the past"
-            except ValueError:
-                errors["dob"] = "Invalid date format. Please use YYYY-MM-DD."
+                    errors["dob_past"] = "Date of Birth must be in the past"
+                elif age < 13:
+                    errors["dob"] = "Age must be at least 13 years"
         #validated format of mail and unique email used
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):             
             errors['email'] = "Invalid email address!"
         if User.objects.filter(email=postData['email']).exists():
-            errors['email'] = "Email already in use!"
+            errors['email_used'] = "Email already in use!"
         # validated pass to be greater than 8 char and match with confirm pass 
         if len(postData['password']) < 8:
             errors["password"] = "Password should be at least 8 characters"
@@ -50,6 +50,7 @@ class UserManager(models.Manager):
             return errors
 
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -62,16 +63,16 @@ class User(models.Model):
     objects = UserManager()
 
 # create user 
-def create_user(session):
+def create_user(POST):
     password = POST['password']
     return User.objects.create(
-        first_name=session['first_name'],
-        last_name =session['last_name'],
-        email=session['email'],
-        dob=session['dob'],
+        first_name=POST['first_name'],
+        last_name =POST['last_name'],
+        email=POST['email'],
+        dob=POST['dob'],
         password= bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode(),
         # confirm_pw=POST['password']
         )
 
 def get_user(session):
-    User.objects.get(id=session['user_id'])
+    return User.objects.get(id=session['user_id'])
